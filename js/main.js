77f2534640c4bcaab139c3bfd1cb7be642c8a5e9
premdav <prem.dav@outlook.com>
2018-05-14 23:04:13 -0500

    API works correctly. Data is now dynamically populated based on user's location.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,56 @@
 $(document).ready(function () {
-        // getting location data
         if(navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(getWeather);
         }
         else {
-            alert("Geolocation not available");
+            alert("Geolocation not currently available");
         }
 
         function getWeather(position) {
-            // preset latitude and longitude in case of geolocation failure
-            let lat = 48.8566;
-            let lon = 2.3522;
-
             // set latitude and longitude to user's location
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
             // output variables to console
-            console.log(lat, lon);
-            let urlApi = "https://fcc-weather-api.glitch.me/api/current?long=" + lon
-                + "&lat=" + lat;
+            console.log(lat + ',' + lon);
+            let urlApi = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat
+                + "&lon=" + lon;
             // logging url to the console
             console.log(urlApi);
-        }
+            $.getJSON(urlApi, function(json) {
+                console.log(json);
+                $(".cityName").html(json.name);
+                $(".temp").html(json.main.temp + "&deg;C");
+                $(".weather").html(json.weather[0].description);
 
-        //function currentWeather(data) {
-          //  console.log(data);
-            //$(".city").html(data.name + ", " + data.sys.country);
-        //}
+                let myWeather = json.weather[0].description;
+                if(myWeather === "clear sky") {
+                    $(".my-icon").addClass("wi-sunny");
+                }
+                else if(myWeather === "few clouds") {
+                    $(".my-icon").addClass("wi-day-cloudy");
+                }
+                else if(myWeather === "scattered clouds") {
+                    $(".my-icon").addClass("wi-cloud");
+                }
+                else if(myWeather === "shower rain") {
+                    $(".my-icon").addClass("wi-showers");
+                }
+                else if(myWeather === "broken clouds") {
+                    $(".my-icon").addClass("wi-cloudy");
+                }
+                else if(myWeather === "rain") {
+                    $(".my-icon").addClass("wi-rain");
+                }
+                else if(myWeather === "thunderstorm") {
+                    $(".my-icon").addClass("wi-thunderstorm");
+                }
+                else if(myWeather === "snow") {
+                    $(".my-icon").addClass("wi-snow");
+                }
+                else if(myWeather === "mist") {
+                    $(".my-icon").addClass("wi-fog");
+                }
+
+            });
+        }
 });
